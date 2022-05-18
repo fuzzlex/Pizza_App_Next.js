0d2e28837e4db567d4809db03c0fdd9e68994821
emre <emrky.2018@icloud.com>
2022-05-19 00:28:34 +0300

    first comm

---
+++ components/Navbar.jsx
@@ -0,0 +1,15 @@
+import styles from "../styles/Navbar.module.css"
+
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+        <div>
+        
+
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
